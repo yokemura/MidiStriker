32bbbcdb4e759b6eed39f8365c6d8fbcb7ac07da
Takeshi Yokemura <yokemura@ymck.net>
2022-10-22 20:48:36 +0900

    Initial Commit

---
+++ MidiStriker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MidiStriker
+//
+//  Created by 除村武志 on 2022/10/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
